66e7ba503840030aa5b9f0373464e63d4fe1d2e5
mlewis42 <37189623+mlewis42@users.noreply.github.com>
2022-12-10 23:41:22 -0500

    multi initial setup

---
+++ Glass Crown_clone_0/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal * .2f, moveVertical * .2f, 0);
+            transform.position = transform.position + movement;
+
+
+        }
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
